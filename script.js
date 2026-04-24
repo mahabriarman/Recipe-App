d26dedc1b5fb31130e9928031fd4211adf1dcd9d
Arman Mahabri <mahabriarman@gmail.com>
2026-04-24 22:36:26 +0530

    improve app functionality and enhance UI/UX

--- script.js
+++ script.js
@@ -4,21 +4,28 @@ const RecipeContainer = document.querySelector(".recipe-container")
 
 
 const fetchRecipe = async(qurey)=>{
+    RecipeContainer.innerHTML="<h2>Fetahing Recipes.....<h2>"
      const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${qurey}`)
      const response = await data.json()
+
+     RecipeContainer.innerHTML=""
     response.meals.forEach(meal => {
         const recipediv = document.createElement('div')
         recipediv.classList.add('recipe')
         recipediv.innerHTML=`
         <img src="${meal.strMealThumb}"/>
         <h3>${meal.strMeal}</h3>
-        <p>${meal.strArea}</p>
-        <p>${meal.strCategory}</p>
+        <p><span>${meal.strArea}</span> Dish</p>
+        <p>Belong to <span>${meal.strCategory}</span> Category</p>
         `
+        const button = document.createElement('button')
+        button.textContent="View Recipe"
+        recipediv.appendChild(button)
         RecipeContainer.appendChild(recipediv)
     });
     }
 
+    
 Serachbtn.addEventListener("click" ,(e) => {
     e.preventDefault()
     const serachInput = Serachbox.value.trim()
